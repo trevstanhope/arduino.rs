df418d07acce017352e9c64cbbb8d47bb8fb8b62
Trevor Stanhope <trevor@firebase>
2015-09-05 03:39:59 -0400

    write LED

--- sketches/foobar/foobar.ino
+++ sketches/foobar/foobar.ino
@@ -3,11 +3,14 @@ char MESSAGE[] = "{\"foo\":0,\"bar\":1}";
 
 void setup() {
   Serial.begin(BAUD);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(13, HIGH);
   if (Serial.available() > 0) {
     Serial.read();
     Serial.println(MESSAGE);
   }
+  digitalWrite(13, LOW);
 }
